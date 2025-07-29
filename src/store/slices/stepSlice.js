1d0be77b99b476a70eb4499fd16ba96735ad6e1a
BahaaDagher <bahaa.dagher@softprograms-eg.com>
2025-07-29 08:48:36 +0300

    remove debugger

--- src/store/slices/stepSlice.js
+++ src/store/slices/stepSlice.js
@@ -72,7 +72,7 @@ export const deleteStep = createAsyncThunk(
     "step/deleteStep", 
     async (values) => {
         try {
-            debugger;
+            // debugger;
             const token = localStorage.getItem('token');
             const response = await axios.post(
             `https://test.secretvisitor.co/dashboard/api/steps/${values.id}/delete` ,{
